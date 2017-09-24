a2ecfbc0c76edccc494a9aee7735a31fea0e25dd
Boo Kai Hsien <christopherboo66@gmail.com>
2017-09-24 17:52:10 +0800

    update solution for week 6 problem 2

--- programs/6/p2.js
+++ programs/6/p2.js
@@ -1,10 +1,11 @@
 function makeup_amount(x, l) {
     if (is_pair(l)) {
-        return append(makeup_amount(x - head(l), tail(l)),
+        return append(map(function(y) { return append(list(head(l)), y); },
+                          makeup_amount(x - head(l), tail(l))),
                       makeup_amount(x, tail(l)));
     } else if (x === 0) {
         return list([]);
     } else {
-        return []
+        return [];
     }
 }
